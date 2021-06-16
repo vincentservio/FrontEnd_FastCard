6a2da9880601a29e47ae2c80711ea84b1de729be
vincentservio <vincentservio@gmail.com>
2021-06-15 23:57:32 -0400

    cards with css

--- front_end_fast_card/index.js
+++ front_end_fast_card/index.js
@@ -1,34 +1,27 @@
+const loadData = () => {
+  fetch("http://localhost:3001/decks")
+    .then((resp) => resp.json())
+    .then((decks) => {
+      debbugger;
+    });
+};
 const landingPage = () => {
   const main = document.getElementById("main");
+
   main.innerHTML += `
-  <div id="container">
+<h1>Card Flip with Text</h1>
+<h3>Hover over the image below:</h3>
 
-    <!-- A div with card class for the card  -->
-    <div class="card">
-      <img src="https://images.unsplash.com/photo-1536323760109-ca8c07450053" alt="Lago di Braies">
-
-      <!-- A div with card__details class to hold the details in the card  -->
-      <div class="card__details">
-
-        <!-- Span with tag class for the tag -->
-        <span class="tag">Nature</span>
-
-        <span class="tag">Lake</span>
-
-        <!-- A div with name class for the name of the card -->
-        <div class="name">Lago di Braies</div>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur sodales morbi dignissim sed diam
-          pharetra
-          vitae ipsum odio.</p>
-
-        <button>Read more</button>
-      </div>
-
-
-    </div>
+<div class="scene scene--card">
+  <div class="card">
+    <div class="card__face card__face--front">front</div>
+    <div class="card__face card__face--back">back</div>
   </div>
- 
+</div>
  `;
+  var card = document.querySelector(".card");
+  card.addEventListener("click", function () {
+    card.classList.toggle("is-flipped");
+  });
 };
 landingPage();
