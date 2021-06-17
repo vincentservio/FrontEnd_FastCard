bb3bd0b565ef5fa9157823917a3021862d04ec03
vincentservio <vincentservio@gmail.com>
2021-06-16 23:53:49 -0400

    propercard format

--- front_end_fast_card/index.js
+++ front_end_fast_card/index.js
@@ -2,10 +2,34 @@ const loadData = () => {
   fetch("http://localhost:3001/decks")
     .then((resp) => resp.json())
     .then((decks) => {
-      debbugger;
+      debugger;
+      decks.forEach((deck) => {
+        debugger
+            const main = document.getElementById("main");
+
+            main.innerHTML += `
+<h1>Card Flip with Text</h1>
+<h3>Hover over the image below:</h3>
+
+<div class="scene scene--card">
+  <div class="card">
+    <div class="card__face card__face--front">front</div>
+    <div class="card__face card__face--back">back</div>
+  </div>
+</div>
+ `;
+            var card = document.querySelector(".card");
+            card.addEventListener("click", function () {
+              card.classList.toggle("is-flipped");
+            });
+
+
+
+        });
+      });
     });
 };
-const landingPage = () => {
+const chooseDeck = () => {
   const main = document.getElementById("main");
 
   main.innerHTML += `
@@ -24,4 +48,5 @@ const landingPage = () => {
     card.classList.toggle("is-flipped");
   });
 };
-landingPage();
+chooseDeck();
+loadData();
