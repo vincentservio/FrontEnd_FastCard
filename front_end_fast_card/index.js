fd598fbee1c75f09c9a78e0f1cb1d282005b4ad6
vincentservio <vincentservio@gmail.com>
2021-06-17 00:00:04 -0400

    .

--- front_end_fast_card/index.js
+++ front_end_fast_card/index.js
@@ -4,27 +4,23 @@ const loadData = () => {
     .then((decks) => {
       debugger;
       decks.forEach((deck) => {
-        debugger
-            const main = document.getElementById("main");
+        const main = document.getElementById("main");
+        debugger;
 
-            main.innerHTML += `
+        main.innerHTML += `
 <h1>Card Flip with Text</h1>
 <h3>Hover over the image below:</h3>
 
 <div class="scene scene--card">
   <div class="card">
-    <div class="card__face card__face--front">front</div>
+    <div class="card__face card__face--front">${deck.name}</div>
     <div class="card__face card__face--back">back</div>
   </div>
 </div>
  `;
-            var card = document.querySelector(".card");
-            card.addEventListener("click", function () {
-              card.classList.toggle("is-flipped");
-            });
-
-
-
+        var card = document.querySelector(".card");
+        card.addEventListener("click", function () {
+          card.classList.toggle("is-flipped");
         });
       });
     });
