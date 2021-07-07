564c82a93e4f20e18f15899a83eaa781cfcbdef3
vincentservio <vincentservio@gmail.com>
2021-07-06 23:18:48 -0400

    added deck

---
+++ deck.js
@@ -0,0 +1,57 @@
+const getDecks = () => {
+  clearUls();
+  clearForm();
+
+  let showDecks = document.getElementById("show-decks");
+  let CardBtn = document.getElementById("CardBtn");
+  CardBtn.innerHTML = "";
+  fetch(BASE_URL + "/decks")
+    .then((resp) => resp.json())
+    .then((decks) => {
+      document.getElementById("sortDecks").addEventListener("click", () => {
+        sortDecks(decks);
+      });
+      decks.forEach((deck) => {
+        let dk = new Deck(deck);
+        // attachClickToLinks()
+        showDecks.innerHTML += dk.renderDeck();
+      });
+      attachCltickToDeck();
+    });
+};
+
+function sortDecks() {
+  clearUls();
+  let showDecks = document.getElementById("show-decks");
+  fetch(BASE_URL + "/decks")
+    .then((resp) => resp.json())
+    .then((decks) => {
+      decks.sort(function (a, b) {
+        let categoryA = a.category.toUpperCase();
+        let categoryB = b.category.toUpperCase();
+        return categoryA > categoryB ? -1 : 1;
+      });
+      decks.forEach((deck) => {
+        let dk = new Deck(deck);
+        attachClickToDeck();
+        showDecks.innerHTML += dk.renderDeck();
+      });
+    });
+}
+Deck.decks = [];
+class Deck {
+  constructor(deck) {
+    this.id = deck.id;
+    this.category = deck.category;
+    Deck.decks << this;
+  }
+
+  renderDeck() {
+    return `
+    <li>
+        <a href="#" data-id="${this.id}">${this.category}</a>
+    </li>
+    <br>
+    `;
+  }
+}
