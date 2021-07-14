4ccd87eb5b68b7bc5172a565204be1798aebab1e
vincentservio <vincentservio@gmail.com>
2021-07-13 23:42:41 -0400

    v2

--- front_end_fast_card/deckCrud.js
+++ front_end_fast_card/deckCrud.js
@@ -1,47 +1,44 @@
-
-
 //^============== create deck Form =============^
 
 const displayCreateDeckForm = () => {
-    let cardFormDiv = document.getElementById('card-form')
-    let html = `
+  let cardFormDiv = document.getElementById("card-form");
+  let main = `
         <form>
             <input type="text" id="category" placeholder="Category">
             <br>
             <input type="submit"> 
          </form>
-    `
-    cardFormDiv.innerHTML = html
-    document.querySelector('form').addEventListener('submit', () => {
-        createDeck()
-        clearForm()
-        getDecks()
-    })
-}
+    `;
+  cardFormDiv.innerHTML = main;
+  document.querySelector("form").addEventListener("submit", () => {
+    createDeck();
+    clearForm();
+    getDecks();
+  });
+};
 
 //^============== create deck =============^
-const createDeck = () =>  {
-    event.preventDefault()
-    const deck = {
-      category: document.getElementById('category').value
-    }
-    fetch(BASE_URL + '/decks', {
-        method: "POST",
-        body: JSON.stringify(deck),
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    })
-    .then(resp => resp.json())
-    .then(deck => {
-        document.querySelector('#show-cards').innerHTML += `
+const createDeck = () => {
+  event.preventDefault();
+  const deck = {
+    category: document.getElementById("category").value,
+  };
+  fetch(BASE_URL + "/decks", {
+    method: "POST",
+    body: JSON.stringify(deck),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((deck) => {
+      document.querySelector("#show-cards").innerHTML += `
         <li>
             <a href="#" data-id="${deck.id}">${deck.category}
         </li>
-        `
-        attachClickToLinks()
-        clearForm()
-    })
-}
-
+        `;
+      attachClickToLinks();
+      clearForm();
+    });
+};
